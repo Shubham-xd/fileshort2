4a04b57735ad08b5eb6910869cf573c1f9c2f5c4
Tamilgram <107699896+Tamilgram@users.noreply.github.com>
2022-11-26 20:10:33 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,8 +7,8 @@ import os
 
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "0"))
-	API_HASH = "d61fc99a7dcba3c40767e8072a18511b"
+        API_ID = int(os.environ.get("API_ID", "3304796"))
+        API_HASH = os.environ.get("API_HASH", "d61fc99a7dcba3c40767e8072a18511b")
 	BOT_TOKEN = os.environ.get("BOT_TOKEN")
 	BOT_USERNAME = os.environ.get("BOT_USERNAME")
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
